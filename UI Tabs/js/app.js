dcba0bbdfe75ba92f1522623f171f19e4de5ee74
Alexandra Caulea <alexandra.caulea@gmail.com>
2020-05-13 11:19:42 +0300

    Remove js comments

--- UI Tabs/js/app.js
+++ UI Tabs/js/app.js
@@ -3,16 +3,10 @@ const tabButtons = tabs.querySelectorAll("[role='tab']");
 const tabPanels = Array.from(tabs.querySelectorAll("[role='tabpanel']"));
 
 function tabClickHandler(e) {
-  // 1. Hide all tabPanels
   tabPanels.forEach(panel => (panel.hidden = true));
-  // 2. Unselect all tabButtons
   tabButtons.forEach(button => button.setAttribute("aria-selected", false));
-  // 3. Mark the button that was clicked as selected
   event.currentTarget.setAttribute("aria-selected", true);
-  // 4. Find the id of the button that was clicked
   const { id } = event.currentTarget;
-  console.log(id);
-  // 5. Show the panel
   const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
   tabPanel.hidden = false;
 }
